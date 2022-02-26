7ff414c93d4f5c7380719062a9fb4dee3b608802
Julie Sarmiento <ja.sarmiento@uniandes.edu.co>
2022-02-26 15:36:06 -0500

    Se implementa validacion de los campos del formulario del login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,30 +5,62 @@
         </div>
         <div class="col-md-4 col-sm-12">
             <div class="login-form">
-                <form>
+                <Form @submit="onSubmit">
                     <div class="form-group">
                         <h1><i class="las la-users"></i> TalkFriends</h1>
                     </div>
                     <div class="form-group pb-2">
-                        <label>Usuario</label>
-                        <input type="text" class="form-control">
+                        <label>Email</label>
+                        <Field type="email" class="form-control" name="email" :rules="validateEmail" />
+                        <ErrorMessage name="email" class="text-danger"/>
                     </div>
                     <div class="form-group pb-3">
                         <label>Contraseña</label>
-                        <input type="password" class="form-control">
+                        <Field type="password" class="form-control" name="password" :rules="validatePassword"/>
+                        <ErrorMessage name="password" class="text-danger"/>
                     </div>
                     <button type="submit" class="btn btn-primary m-2">Ingresar</button>
                     <button type="submit" class="btn btn-light">Registrarse</button>
-                </form>
+                </Form>
             </div>
         </div>
   </div>
 </template>
 
 <script>
-export default {
+    import { Form, Field, ErrorMessage } from 'vee-validate';
 
-}
+    export default {
+        components: {
+            Form,
+            Field,
+            ErrorMessage 
+        },
+        methods: {
+            onSubmit(values) {
+                console.log(JSON.stringify(values, null, 2));
+            },
+            validateEmail(value) {
+                if (!value) {
+                    return 'Este campo es requerido';
+                }
+                
+                const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+                if (!regex.test(value)) {
+                    return 'Este campo debe ser un Email valido';
+                }
+                
+                return true;
+            },
+            validatePassword(value){
+                if (!value) {
+                    return 'Este campo es requerido';
+                }
+
+                return true;
+            }
+        },
+    }
 </script>
 
 <style>
